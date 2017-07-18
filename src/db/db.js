e287a868c5228f3dd0fd8df52c3c4c42ab039278
Punit Rathore <punitr@gmail.com>
2017-07-18 14:13:25 -0700

    fixing db name

--- src/db/db.js
+++ src/db/db.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')()
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/contacts'
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/contacts_development'
 const db = pgp(connectionString)
 
 module.exports = db
