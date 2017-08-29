d9e2edfefb6468ab0aed181ee8e77ee33b2e6ef4
zezemanolo <manoloish@gmail.com>
2017-08-29 15:41:53 -0700

    adds users seed data

---
+++ test/seed/users.sql
@@ -0,0 +1,7 @@
+INSERT INTO
+  users (email, password, admin)
+VALUES
+  ('yolo@gmail.com', 'password123', false),
+  ('solo@gmail.com', 'pass12345', true),
+  ('nolo@gmail.com', '123pass', false)
+;
