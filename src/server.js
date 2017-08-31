b88122e02c09cbca45dcb5810176754b0699d919
zezemanolo <manoloish@gmail.com>
2017-08-31 16:29:38 -0700

    separates middleware to its own file

--- src/server.js
+++ src/server.js
@@ -1,33 +1,18 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const path = require('path')
-const dbContacts = require('./models/db/contacts')
-const expressSession = require('express-session')
-const app = express()
-const {renderError} = require('./server/utils')
 const routes = require('./server/routes');
+const middleware = require('./server/middleware')
+
+const app = express()
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views/pages')
 
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use((request, response, next) => {
-  response.locals.query = ''
-  response.locals.error = ''
-  response.locals.user = null
-  next()
-})
-app.use(expressSession({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    expires: 600000
-    }
-}))
-
+app.use('/', middleware)
 app.use('/', routes)
+
+app.use((request, response) => {
+  response.render('not_found')
+})
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
